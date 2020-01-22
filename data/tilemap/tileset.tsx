a9d71c02372ee675506694f4bf74ee36913b90ec
Alek Westover <alek.westover@gmail.com>
2020-01-22 15:46:43 -0500

    made a cool tilemap

--- data/tilemap/tileset.tsx
+++ data/tilemap/tileset.tsx
@@ -1,22 +1,46 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="tileset" tilewidth="66" tileheight="60" tilecount="3" columns="0">
+<tileset version="1.2" tiledversion="1.2.4" name="tileset" tilewidth="32" tileheight="32" tilecount="7" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
    <property name="name" value="collision"/>
   </properties>
-  <image width="32" height="32" source="redtile.png"/>
+  <image width="32" height="32" source="gimpTiles/collision.png"/>
  </tile>
  <tile id="1">
   <properties>
-   <property name="name" value="vine"/>
+   <property name="name" value="black"/>
   </properties>
-  <image width="32" height="32" source="tilevines.jpeg"/>
+  <image width="32" height="32" source="gimpTiles/black.png"/>
  </tile>
  <tile id="2">
   <properties>
-   <property name="name" value="dog"/>
+   <property name="name" value="brown"/>
   </properties>
-  <image width="66" height="60" source="../../../../Documents/dogProfilePic.png"/>
+  <image width="32" height="32" source="gimpTiles/brown.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="darkblue"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/darkblue.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="green"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/green.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="lightblue"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/lightblue.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="red"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/red.png"/>
  </tile>
 </tileset>
