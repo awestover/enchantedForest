4ac56394a1f9488a0e4910f83d949a7f3ad10fcc
Kaisu <kevin.hsukaihao@gmail.com>
2020-06-22 14:57:10 -0700

    Ported to flask backend (without mongodb)

---
+++ src/static/data/maps/tileset.tsx
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tileset" tilewidth="32" tileheight="32" tilecount="17" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="collision"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/collision.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="black"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/black.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="brown"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/brown.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="darkblue"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/darkblue.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="green"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/green.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="lightblue"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/lightblue.png"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="red"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/red.png"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="name" value="npc:dawg"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/dogProfilePic.png"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" value="teleporter"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/teleporter.png"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="name" value="mob:jumpydude"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/mobSpawnPoint.png"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="name" value="item:dragonInsignia"/>
+  </properties>
+  <image width="32" height="32" source="../items/dragonInsignia.png"/>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="item:gem"/>
+  </properties>
+  <image width="32" height="32" source="../items/gem.png"/>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="name" value="item:potion"/>
+  </properties>
+  <image width="32" height="32" source="../items/potion.png"/>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="npc:bear"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/bear.png"/>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="name" value="mob:unicorn"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/unicornSpawnPoint.png"/>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="name" value="mob:ninja"/>
+  </properties>
+  <image width="32" height="32" source="gimpTiles/ninjaSpawnPoint.png"/>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="name" value="mob:boar"/>
+  </properties>
+  <image width="32" height="25" source="gimpTiles/boarSpawnPoint.png"/>
+ </tile>
+</tileset>
