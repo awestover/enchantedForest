e8b174b4803873c5546e9cd5dcc621c446499cda
Alek Westover <alek.westover@gmail.com>
2020-08-14 20:37:43 -0400

    fix

--- src/static/js/Mob.js
+++ src/static/js/Mob.js
@@ -5,8 +5,7 @@ class Mob extends Entity {
 
   die(){
     $.notify("You seem a decent fellow. I hate to die.", "info");
-    items.push(new Item(this.pos.x+50, this.pos.y, "potion", "item"));
-    // items.push(new Entity(this.pos.x+50, this.pos.y, "potion", "item"));
+    items.push(new Item(this.pos.x+50, this.pos.y, "potion"));
   }
 
 }
